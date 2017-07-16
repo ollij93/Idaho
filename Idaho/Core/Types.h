3fae94d929ed93900ea9101fbed7ebdd01a8cc4b
Olli Johnson <ollij93@outlook.com>
2017-07-16 21:40:12 +0100

    Created a compile-time version of the GetHash function

--- Idaho/Core/Types.h
+++ Idaho/Core/Types.h
@@ -7,15 +7,23 @@ const Hash uHASH_UNSET = 0;
 template<typename T>
 static Hash GetHash(const T* pszString)
 {
-    if (!pszString) { return uHASH_UNSET; }
-
     Hash uHash = uHASH_UNSET;
-    for (u_int u = 0; pszString[u]; u++) {
-        uHash *= 31;
-        uHash += pszString[u];
+    for (int i = 0; pszString[i]; ++i) {
+        uHash = (uHash * 31) + pszString[i];
     }
+
     return uHash;
 }
+
+template<int iSize>
+constexpr Hash __gethash(const char* pszString)
+{
+    return (__gethash<iSize - 1>(pszString) * 31) + pszString[iSize];
+}
+// This is the stop-recursion function
+template<>
+constexpr Hash __gethash<int(-1)>(const char* pszString) { return 0; }
+#define CompileTimeGetHash(x) (__gethash<sizeof(x) - 2>(x))
 
 struct Color {
     float fRed;
