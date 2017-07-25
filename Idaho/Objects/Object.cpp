54017d14a6c4593cf1ac20248c9a4d8c2e666819
Olli Johnson <ollij93@outlook.com>
2017-07-25 13:51:05 +0100

    Updated project filters and Changes to use CompileTimeGetHash

--- Idaho/Objects/Object.cpp
+++ Idaho/Objects/Object.cpp
@@ -6,7 +6,7 @@ void
 Object::InitFromSpecification(const Specification* pxSpecification)
 {
     ColliderType eType;
-    eType = (ColliderType)pxSpecification->GetValue<u_int>(GetHash("Collider"), ColliderType::UNDEFINED);
+    eType = (ColliderType)pxSpecification->GetValue<u_int>(CompileTimeGetHash("Collider"), ColliderType::UNDEFINED);
 
     ASSERT(eType != CONVEX_MESH && eType != CONCAVE_MESH && eType != HEIGHTFIELD, "Type of collider not yet defined.");
     if (eType == CONVEX_MESH || eType == CONCAVE_MESH || eType == HEIGHTFIELD) { eType = UNDEFINED; }
