d5b4d3221632e41b3cf92fdd2d4bb2239ef63d39
Olli Johnson <ollij93@outlook.com>
2017-07-25 13:51:05 +0100

    Updated project filters and Changes to use CompileTimeGetHash

--- Idaho/Objects/StaticObject.cpp
+++ Idaho/Objects/StaticObject.cpp
@@ -8,8 +8,8 @@ StaticObject::InitFromSpecification(const Specification* pxSpecification)
     Hash uModelHash;
     Hash uTextureHash;
 
-    uModelHash = pxSpecification->GetValue<Hash>(GetHash("Model"), uHASH_UNSET);
-    uTextureHash = pxSpecification->GetValue<Hash>(GetHash("Texture"), uHASH_UNSET);
+    uModelHash = pxSpecification->GetValue<Hash>(CompileTimeGetHash("Model"), uHASH_UNSET);
+    uTextureHash = pxSpecification->GetValue<Hash>(CompileTimeGetHash("Texture"), uHASH_UNSET);
 
     m_xModel.SetModelHash(uModelHash);
     m_xModel.SetTextureHash(uTextureHash);
